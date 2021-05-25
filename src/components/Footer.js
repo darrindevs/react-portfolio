f724dd98a03735d8fcd7eac366e53d8a5368ffeb
Zaino <hello@darrin.dev>
2021-05-25 14:37:44 -0700

    update scripts

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -76,7 +76,7 @@ export default function Footer() {
           ))}
         </div>
         <div className="mt-8 md:mt-0 md:order-1">
-          <p className="text-center text-base text-gray-400">&copy; 2020 Workflow, Inc. All rights reserved.</p>
+          <p className="text-center text-base text-gray-400">&copy; 2021 darrindevs, All rights reserved.</p>
         </div>
       </div>
     </footer>
