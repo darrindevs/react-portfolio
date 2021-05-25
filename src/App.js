4e4ffb901d1706c319cdb7f5e9b03eab2b65a3a2
Zaino <hello@darrin.dev>
2021-05-25 14:59:25 -0700

    test gh pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+//import { BrowserRouter as Router, Route } from "react-router-dom";
 import Container from "./components/Container";
 //import NavTabs from "./components/NavTabs";
 import Navbar from "./components/Navbar";
@@ -22,6 +23,7 @@ import Contact from "./pages/Contact";
 function App() {
   return (
     <Router>
+      <Switch>
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <body class="flex flex-col min-h-screen">{/* keep this for sticky footer */}
         <Navbar />
@@ -37,6 +39,9 @@ function App() {
         <Footer />
         </body>
       </div>
+
+      </Switch>
+      
     </Router>
   );
 }
